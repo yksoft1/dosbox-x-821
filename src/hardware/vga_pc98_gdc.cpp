99d6ceed24b201403def3b8433f99becd735822e
yksoft1 <yksoft1@gmail.com>
2018-03-10 00:55:36 +0800

    According to test against NP2, GDC memory 0x54D 5MHz flag is wrong..

--- src/hardware/vga_pc98_gdc.cpp
+++ src/hardware/vga_pc98_gdc.cpp
@@ -545,7 +545,7 @@ void GDC_ProcDelay(Bitu /*val*/) {
 }
 
 void gdc_5mhz_mode_update_vars(void) {
-    mem_writeb(0x54D,(mem_readb(0x54D) & (~0x04)) | (gdc_5mhz_mode ? 0x04 : 0x00));
+    mem_writeb(0x54D,(mem_readb(0x54D) & (~0x20)) | (gdc_5mhz_mode ? 0x20 : 0x00));
 
 }
 
