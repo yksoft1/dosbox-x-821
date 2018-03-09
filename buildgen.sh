9987c0bcb08046815e879e0b51000995e3a25e1c
yksoft1 <yksoft1@gmail.com>
2018-03-09 23:03:28 +0800

    vs2015 files should be left intact.

--- buildgen.sh
+++ buildgen.sh
@@ -4,7 +4,13 @@ export CFLAGS="-O3 -static-libgcc -flto -fno-use-linker-plugin"
 export CXXFLAGS="-O3 -static-libgcc -static-libstdc++ -flto -fno-use-linker-plugin -DWINVER=0x0501 -D_WIN32_WINNT=0x0501"
 
 cd vs2015/sdl
-configure --prefix=/mingw --disable-shared --enable-static
+if test -f configure; then
+	configure --prefix=/mingw --disable-shared --enable-static
+else
+	bash autogen.sh
+	configure --prefix=/mingw --disable-shared --enable-static
+fi
+
 make -j4
 make install
 
