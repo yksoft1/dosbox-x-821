115db0dca596325a8724b903110b2ba6adc987b6
yksoft1 <yksoft1@gmail.com>
2018-03-11 19:27:34 +0800

    Merge johncampbell123's fix to PC-98 DOS INT 21 RTC routines; HX Build shouldn't link with SDL_net.

--- buildhx.sh
+++ buildhx.sh
@@ -15,12 +15,14 @@ make -j4
 make install
 
 cd ../../
+mv /mingw/lib/libSDL_net.a /mingw/lib/libSDL_net.a1
 if test -f configure; then
 	configure --disable-mt32 --disable-opengl
 else
 	bash autogen.sh
 	configure --disable-mt32 --disable-opengl
 fi
+mv /mingw/lib/libSDL_net.a1 /mingw/lib/libSDL_net.a
 make -j4
 cd src
 strip dosbox-x.exe
