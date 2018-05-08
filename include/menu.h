df31cde5a53e69d903e061856dd0d7252eda735d
yksoft1 <yksoft1@gmail.com>
2018-05-08 14:25:42 +0800

    More about sdl_gui updates.

--- include/menu.h
+++ include/menu.h
@@ -41,7 +41,6 @@ void MountDrive_2(char drive, const char drive2[DOS_PATHLENGTH], std::string dri
 void MENU_Check_Drive(HMENU handle, int cdrom, int floppy, int local, int image, int automount, int umount, char drive);
 bool MENU_SetBool(std::string secname, std::string value);
 void MENU_swapstereo(bool enabled);
-void GUI_Shortcut(int select);
 void* GetSetSDLValue(int isget, std::string target, void* setval);
 void Go_Boot(const char boot_drive[_MAX_DRIVE]);
 void Go_Boot2(const char boot_drive[_MAX_DRIVE]);
@@ -95,3 +94,5 @@ extern void GetDefaultSize(void);
 #define DOSBox_Kor() !strncmp("ko", getenv("LANG"), 2) // dirty hack.
 
 #endif
+
+void GUI_Shortcut(int select);
