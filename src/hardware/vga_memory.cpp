2c05edaf90c901bcc0616453db95e69c4445105f
yksoft1 <yksoft1@gmail.com>
2019-02-03 17:57:15 +0800

    Fix PC-98 Port 6A EGC enable and GRCG Mode 8/C write (joncampbell123).

--- src/hardware/vga_memory.cpp
+++ src/hardware/vga_memory.cpp
@@ -1399,22 +1399,28 @@ public:
     }
 
     template <class AWT> static inline void mode8_w(const unsigned int plane,const PhysPt vramoff) {
+		AWT tb;
+		
         /* Neko Project II code suggests that the first byte is repeated. */
         if (sizeof(AWT) > 1)
-            pc98_gdc_tiles[plane].b[1] = pc98_gdc_tiles[plane].b[0];
+            tb = pc98_gdc_tiles[plane].b[0] | (pc98_gdc_tiles[plane].b[0] << 8u);
+        else
+            tb = pc98_gdc_tiles[plane].b[0];
 
-        *((AWT*)(vga.mem.linear + vramoff)) = *((AWT*)pc98_gdc_tiles[plane].b);
+        *((AWT*)(vga.mem.linear + vramoff)) = tb;
     }
 
     template <class AWT> static inline void modeC_w(const unsigned int plane,const PhysPt vramoff,const AWT mask,const AWT val) {
-        AWT t;
+        AWT t, tb;
 
         /* Neko Project II code suggests that the first byte is repeated. */
         if (sizeof(AWT) > 1)
-            pc98_gdc_tiles[plane].b[1] = pc98_gdc_tiles[plane].b[0];
+            tb = pc98_gdc_tiles[plane].b[0] | (pc98_gdc_tiles[plane].b[0] << 8u);
+        else
+            tb = pc98_gdc_tiles[plane].b[0];
 
         t  = *((AWT*)(vga.mem.linear + vramoff)) & mask;
-        t |= val & *((AWT*)pc98_gdc_tiles[plane].b);
+        t |= val & tb;
         *((AWT*)(vga.mem.linear + vramoff)) = t;
     }
 
@@ -1594,7 +1600,14 @@ public:
                 vop_offset = (pc98_gdc_vramop & (1 << VOPBIT_ACCESS)) ? 0x20000 : 0;
                 break;
         };
-
+		
+        /* reminder:
+         *
+         * bit 1: VOPBIT_EGC
+         * bit 0: VOPBIT_ACCESS
+         * From GRGC bits:
+         * bit 3: VOPBIT_GRCG  1=GRGC active  0=GRGC invalid  (from bit 7)
+         * bit 2: VOPBIT_GRCG  1=Read/Modify/Write when writing  0=TCR mode at read, TDW mode at write  (from bit 6) */
         switch (pc98_gdc_vramop & 0xF) {
             case 0x00:
             case 0x01:
@@ -1708,6 +1721,13 @@ public:
                 break;
         };
 
+        /* reminder:
+         *
+         * bit 1: VOPBIT_EGC
+         * bit 0: VOPBIT_ACCESS
+         * From GRGC bits:
+         * bit 3: VOPBIT_GRCG  1=GRGC active  0=GRGC invalid  (from bit 7)
+         * bit 2: VOPBIT_GRCG  1=Read/Modify/Write when writing  0=TCR mode at read, TDW mode at write  (from bit 6) */		
         switch (pc98_gdc_vramop & 0xF) {
             case 0x00:
             case 0x01:
