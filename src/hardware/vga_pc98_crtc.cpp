f17dd7c0da5d417dab9bc6b77f306782c9c508f2
yksoft1 <yksoft1@gmail.com>
2019-04-30 09:37:34 +0800

    Add PC-98 port 68h 0Eh/0Fh (joncampbell123).

--- src/hardware/vga_pc98_crtc.cpp
+++ src/hardware/vga_pc98_crtc.cpp
@@ -21,6 +21,7 @@
 void pc98_update_page_ptrs(void);
 
 extern bool					pc98_attr4_graphic;
+extern bool                 pc98_display_enable;
 extern bool                 pc98_graphics_hide_odd_raster_200line;
 
 bool                        gdc_5mhz_mode = false;
@@ -180,6 +181,9 @@ void pc98_port6A_command_write(unsigned char b) {
 			pc98_256kb_boundary = true;
 			VGA_SetupHandlers(); // memory mapping presented to the CPU changes
 			break;
+		// TODO: 0x82/0x83 GDC Clock #1   0=2.5MHz   1=5MHz
+		// TODO: 0x84/0x85 GDC Clock #2   0=2.5MHz   1=5MHz
+		// TODO: 0x8E/0x8F VRAM use selection  0=PC-98 graphics  1=Cirrus Logic CL-GD graphics   (VRAM is shared?)
         default:
             LOG_MSG("PC-98 port 6Ah unknown command 0x%02x",b);
             break;
@@ -206,11 +210,10 @@ void pc98_port68_command_write(unsigned char b) {
 		case 0x0b:
 			//restorekacmode()
 			break;
-		// TODO: 0x68/0x69 VRAM configuration setting. 0=128KB boundary (32kB per plane)  1=256KB boundary (64kB per plane)
-		//              ^  Needed for 480-line modes, or else there is not enough memory.
-		// TODO: 0x82/0x83 GDC Clock #1   0=2.5MHz   1=5MHz
-		// TODO: 0x84/0x85 GDC Clock #2   0=2.5MHz   1=5MHz
-		// TODO: 0x8E/0x8F VRAM use selection  0=PC-98 graphics  1=Cirrus Logic CL-GD graphics   (VRAM is shared?)
+		case 0x0E: // Display enable
+		case 0x0F:
+			pc98_display_enable = !!(b&1);
+			break;
         default:
             LOG_MSG("PC-98 port 68h unknown command 0x%02x",b);
             break;
